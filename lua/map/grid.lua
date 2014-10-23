3b78d48c0128fb43a731f251698765a40c5a7b9a
Son Hai Nguyen <sirarseus@gmail.com>
2014-10-23 21:39:39 +0200

    Add-Lua: generating Grid

--- lua/map/grid.lua
+++ lua/map/grid.lua
@@ -1,21 +1,26 @@
-Grid = {
-	Width = 9
-	Height = 6
-	MaxCountBlocks = 11
-}
+dofile("unknowsegment.lua")
 
+Grid = {
+	Width = 9,
+	Height = 6,
+	MaxCountBlock = 11
+}
+ 
 function Grid.new()
 	local o = {}
 
 	o.p_Grid = {}
 
-	for y = 1, Grid.Height do
-		o.p_Grid[y] = {}
+	for x = 1, Grid.Width do
+		o.p_Grid[x] = {}
 
-		for x = 1, Grid.Width do
-			o.p_Grid
+		for y = 1, Grid.Height do
+			o.p_Grid[x][y] = UnknowSegment.new(x, y)
+		end
 	end
 
 	setmetatable(o, {__index = Grid})
     return o
 end
+
+
