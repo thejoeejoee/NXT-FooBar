98c905ee0319e3d005b0ef6ef922ad19c223d20a
Son Hai Nguyen <sirarseus@gmail.com>
2014-10-24 21:51:12 +0200

    Add-Lua: metamethod, which does something like isinstance - Segment

--- lua/map/segment.lua
+++ lua/map/segment.lua
@@ -1,12 +1,34 @@
 Segment = {}
 
+Segment_Meta = {
+	__index = Segment,
+
+	__call = function(self, ...)
+		if self.__type == nil then
+			error("exTypeNotDefined")
+		end
+
+		if type(arg) == "table" then
+			for k, v in pairs(arg) do
+				if v == self.__type then
+					return true
+				end
+			end
+		end
+
+		return false
+	end
+}
+
+
 function Segment.new(x, y)
 	local o = {} 
 
 	o.p_X = x or error("exBadArgument")
 	o.p_Y = y or error("exBadArgument")
 	o.p_Position = {x, y}
+	o.__type = nil
 
-	setmetatable(o, {__index = Segment})
+	setmetatable(o, Segment_Meta)
 	return o
 end
