9cee8c0248d7a77c9490a8f864f9fc761bd0fa18
Son Hai Nguyen <sirarseus@gmail.com>
2014-10-25 19:47:10 +0200

    New-Lua: Block class created

---
+++ lua/map/block.lua
@@ -0,0 +1,14 @@
+dofile("segment.lua")
+
+Block = {}
+
+function Block.new(x, y)
+	local o = Segment.new(x, y)
+	local meta = getmetatable(o)
+
+	o.__type = "block"
+	meta.__index = Block
+
+	setmetatable(o, meta)
+	return o
+end
