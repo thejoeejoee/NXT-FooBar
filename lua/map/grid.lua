067234a5fbbb827573e2a23de510f15879289555
Son Hai Nguyen <sirarseus@gmail.com>
2014-10-25 20:18:19 +0200

    Add-Lua: locating functions added - Grid

--- lua/map/grid.lua
+++ lua/map/grid.lua
@@ -1,4 +1,5 @@
 dofile("unknowsegment.lua")
+dofile("directions.lua")
 
 Grid = {
 	Width = 9,
@@ -34,4 +35,45 @@ function Grid:set(position, segment)
 		self.__grid[position[0]][position[1]] = segment.new(position[0], position[1])
 	end
 end
+
+function Grid:position_exits(position)
+	if position[0] < 0 or position[1] < 0 then
+		return false
+	elseif position[0] > Grid.Width - 1 or position[1] > Grid.Height - 1 then
+		return false
+	end
+
+	return true 
+end
+
+function Grid:getNextPosition(side, position)
+	if side == Directions.Top then
+		return {position[0], position[1] - 1}
+	elseif side == Directions.Right then
+		return {position[0] + 1, position[1]}
+	elseif side == Directions.Bottom then
+		return {position[0], position[1] + 1}
+	elseif side == Directions.Left then
+		return {position[0] - 1, position[1]}
+	else
+		error("exBadIndex")
+	end
+end
+
+function Grid:oppositeSide(side)
+	if side == Directions.Top or side == Directions.Right then
+		return side + 2
+	elseif side == Directions.Bottom or side == Directions.Left then
+		return side - 2
+	end
+end
+
+
+
+
+
+
+
+
+
 
