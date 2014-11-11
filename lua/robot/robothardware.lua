eac2b7a0eca680f23706a5579b9e23e9c9dfae96
Son Hai Nguyen <sirarseus@gmail.com>
2014-11-11 15:54:50 +0100

    Add-Lua: move method - RobotHardware

--- lua/robot/robothardware.lua
+++ lua/robot/robothardware.lua
@@ -58,12 +58,68 @@ function RobotHardware:rotateSensor(side)
 	end
 
 	if side < self.__sensorSide then
-		self.__sensorMotor:setSpeed(100, Motor.Backward)
+		self.__sensorMotor:setSpeed(100, Motor.Forward)
 
 	else
-		self.__sensorMotor:setSpeed(100, Motor.Forward)
+		self.__sensorMotor:setSpeed(100, Motor.Backward)
 	end
 
 	self.__sensorMotor:limitedRotate(angle_90 * math.floor(math.abs(side - self.__sensorSide)))
 	self.__sensorSide = side
 end
+
+function RobotHardware:move(side)
+	local verticalRotation = 3*360-37
+	local horizontalRotation = 672
+	
+	self:rotateSensor(side)
+
+	if (side == Sides.Right or side == Sides.Left) and self.__direction == Directions.Vertical then
+		self:changeDirection(Directions.Horizontal)
+
+	elseif (side == Sides.Top or side == Sides.Bottom) and self.__direction == Directions.Horizontal then
+		self:changeDirection(Directions.Vertical)
+	end
+
+	if side == Sides.Right or side == Sides.Top then
+		self.__mainMotor:setSpeed(100, Motor.Backward)
+	else
+		self.__mainMotor:setSpeed(100, Motor.Forward)
+	end
+
+	if self.__direction == Directions.Vertical then
+		self.__mainMotor:limitedRotate(verticalRotation)
+	else
+		self.__mainMotor:limitedRotate(horizontalRotation)
+	end
+
+	self.__delay_1 = true
+
+	repeat
+		try(
+			function()
+				self.__mainMotor:controlRotation()
+				print(self.__mainMotor.__port)
+			end
+		)
+
+		catch("RotationFinished_"..tostring(self.__mainMotor.__port),
+			function()
+				print(3)
+				self.__delay_1 = false
+			end
+		)
+	until(not self.__delay_1)
+end
+
+
+
+
+
+
+
+
+
+
+
+
